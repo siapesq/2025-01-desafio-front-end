67fb37cf99ce89dfca5a384e2768eba03156ac66
jvras58 <jvras@cin.ufpe.br>
2025-04-15 17:04:57 -0300

    feat: adicionar componentes de UI (Alert, Checkbox, Form, Label, Separator, Toaster) e atualizar dependências

--- pesquia/app/layout.tsx
+++ pesquia/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner"
 import { Providers } from "@/providers/providers";
 
 const geistSans = Geist({
@@ -29,7 +30,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Providers>{children}</Providers>
+        <Providers>
+         {children}
+         <Toaster />
+        </Providers>
       </body>
     </html>
   );
